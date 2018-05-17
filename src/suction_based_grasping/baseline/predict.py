ace6a00feb6b0dfa3abb9aee8c364acf7768e189
CoolNamesAllTaken <jkailimcnelly@gmail.com>
2018-05-17 03:26:38 +0000

    initial baseline working

--- src/suction_based_grasping/baseline/predict.py
+++ src/suction_based_grasping/baseline/predict.py
@@ -3,6 +3,14 @@
 
 import numpy as np
 import pcl
+from scipy.ndimage.filters import uniform_filter
+import matplotlib.pylab as plt # FOR TESTING ONLY
+
+# From https://nickc1.github.io/python,/matlab/2016/05/17/Standard-Deviation-(Filters)-in-Matlab-and-Python.html
+def window_stdev(X, window_size):
+    c1 = uniform_filter(X, window_size, mode='reflect')
+    c2 = uniform_filter(X*X, window_size, mode='reflect')
+    return np.sqrt(c2 - c1*c1)
 
 # A baseline algorithm for predicting affordances for suction-based
 # grasping: (1) compute 3D surface normals of the point cloud projected
@@ -11,9 +19,9 @@ import pcl
 #
 # function [affordanceMap,surfaceNormalsMap] = predict(inputColor,inputDepth,backgroundColor,backgroundDepth,cameraIntrinsics)
 # Input:
-#   inputColor         - 480x640x3 float array of RGB color values scaled to range [0,1]
+#   inputColor         - 480x640x3 float array of RGB color values
 #   inputDepth         - 480x640 float array of depth values in meters
-#   backgroundColor    - 480x640x3 float array of RGB color values scaled to range [0,1]
+#   backgroundColor    - 480x640x3 float array of RGB color values 
 #   backgroundDepth    - 480x640 float array of depth values in meters
 #   cameraIntrinsics   - 3x3 camera intrinsics matrix
 # Output:
@@ -21,43 +29,101 @@ import pcl
 #   surfaceNormalsMap  - 480x640x3 float array of surface normals in camera coordinates (meters)
 
 def predict(inputColor, inputDepth, backgroundColor, backgroundDepth, cameraIntrinsics):
+	print("Starting predict...")
 	# Scale color images between [0, 1]
-	inputColor /= 255.
-	backgroundColor /= 255.
+	# inputColor /= 255.
+	# backgroundColor /= 255.
+
+	# plt.figure()
+	# plt.imshow(inputColor)
+	# plt.axis('off')
+
+	# plt.figure()
+	# plt.imshow(inputDepth)
+	# plt.axis('off')
+
+	# plt.figure()
+	# plt.imshow(backgroundColor)
+	# plt.axis('off')
+
+	# plt.figure()
+	# plt.imshow(backgroundDepth)
+	# plt.axis('off')
 	
 	# Do background subtraction to get foreground mask
-	foregroundMaskColor = !(np.sum(np.abs(inputColor - backgroundColor) < 0.3, axis=3) == 3)
-	foregroundMaskDepth = backgroundDepth != 0 and np.abs(inputDepth - backgroundDepth) > 0.02
-	foregroundMask = foregroundMaskColor or foregroundMaskDepth
+	foregroundMaskColor = (np.sum(np.abs(inputColor - backgroundColor) < 0.3, axis=2) != 3) # mask pixels similar to background
+	foregroundMaskDepth = np.logical_and(backgroundDepth != 0, np.abs(inputDepth - backgroundDepth) > 0.02)
+	foregroundMask = np.logical_or(foregroundMaskColor, foregroundMaskDepth)
+
+	# show masked image
+	# plt.figure()
+	# showImg = inputColor
+	# showImg[foregroundMask == False] = 0
+	# plt.imshow(showImg)
+	# plt.axis('off')
 
 	# Project depth into camera space
 	[pixX, pixY] = np.meshgrid(list(range(640)), list(range(480)))
-	camX = (pixX - cameraIntrinsics[0, 2]). * inputDepth / cameraIntrinsics[0, 0]
-	camY = (pixY - cameraIntrinsics[1, 2]). * inputDepth / cameraIntrinsics[1, 1]
+	camX = (pixX - cameraIntrinsics[0, 2]) * inputDepth / cameraIntrinsics[0, 0]
+	camY = (pixY - cameraIntrinsics[1, 2]) * inputDepth / cameraIntrinsics[1, 1]
 	camZ = inputDepth
 
 	# Only use points with valid depth and within foreground mask
-	validDepth = foregroundMask and camZ != 0
-	inputPoints = [camX[validDepth], camY[validDepth], camZ[validDepth]]
+	validDepth = np.logical_and(foregroundMask, camZ != 0)
+	xPoints = camX[validDepth]
+	yPoints = camY[validDepth]
+	zPoints = camZ[validDepth]
+
+	# repackage x, y, z lists into point tuples
+	numPoints = len(xPoints)
+	inputPoints = [(xPoints[i], yPoints[i], zPoints[i]) for i in range(numPoints)]
 	
 	# Get foreground point cloud normals
-	foregroundPointcloud = pcl.PointCloud().from_list(inputPoints)
-	foregroundNormals = foregroundPointcloud.calc_normals(50)
+	foregroundPointcloud = pcl.PointCloud()
+	foregroundPointcloud.from_list(inputPoints)
+	foregroundNormals = calc_surface_normals(foregroundPointcloud)
 
 	# Flip normals to point toward sensor
-	sensorCenter = [0, 0, 0]
-	inputPoints = inputPoints.T
-	for k in range(len(inputPoints[2])):
-		p1 = sensorCenter - [inputPoints[0, k], inputPoints[1, k], inputPoints[2, k]]
-		p2 = [foregroundNormals[k, 0], foregroundNormals[k, 1], foregroundNormals[k, 2]]
+	sensorCenter = np.zeros(3)
+	# did this weird because foregroundNormals has curvature, can't directly np.asarray it
+	foregroundPointcloudArray = np.asarray(foregroundPointcloud)
+	foregroundNormalsList = []
+	for i in range(len(inputPoints)):
+		foregroundNormalsList.append(np.asarray(foregroundNormals[i])[0:3]) # ignore curvature value (index 3)
+	foregroundNormalsArray = np.asarray(foregroundNormalsList)
+	# foregroundNormalsArray = np.asarray([np.asarray(foregroundNormals[i, 0:3]) for i in range(len(inputPoints))])
+	# inputPoints = inputPoints.T
+	for k in range(len(inputPoints)):
+		p1 = sensorCenter - foregroundPointcloudArray[k]
+		p2 = foregroundNormalsArray[k]
 		angle = np.arctan2(p1.dot(p2.T), np.linalg.norm(np.cross(p1, p2)))
 		if angle <= np.pi / 2 and angle >= -np.pi / 2:
-			foregroundNormals[k] *= -1
+			foregroundNormalsArray[k] *= -1
 
 	# Project normals back to image plane
-	pixX = math.round(inputPoints[0] * cameraIntrinsics[0, 0]. / inputPoints[2] + cameraIntrinsics[0, 2])
-	pixY = math.round(inputPoints[1] * cameraIntrinsics[1, 1]. / inputPoints[2] + cameraIntrinsics[1, 2])
+	inputPointsArray = np.asarray(inputPoints)
+	pixX = np.around((inputPointsArray[:, 0] * cameraIntrinsics[0, 0]) / (inputPointsArray[:, 2]) + cameraIntrinsics[0, 2]).astype(int)
+	pixY = np.around((inputPointsArray[:, 1] * cameraIntrinsics[1, 1]) / (inputPointsArray[:, 2]) + cameraIntrinsics[1, 2]).astype(int)
+	# matlab does a weird linear indexing thing here, so just flatten and roll with it
 	surfaceNormalsMap = np.zeros_like(inputColor)
-	surfaceNormalsMap[np.ravel_multi_index(surfaceNormalsMap.size, (pixY, pixX, np.zeros_like(pixY))] = foregroundNormals[:, 0]
-	surfaceNormalsMap[np.ravel_multi_index(surfaceNormalsMap.size, (pixY, pixX, np.ones_like(pixY))] = foregroundNormals[:, 1]	
-	surfaceNormalsMap[np.ravel_multi_index(surfaceNormalsMap.size, (pixY, pixX, 2 * np.ones_like(pixY))] = foregroundNormals[:, 2]
+	surfaceNormalsMapFlat = np.reshape(surfaceNormalsMap, (-1,)) # this is a little redundant but whatever
+	surfaceNormalsMapFlat[np.ravel_multi_index((pixY, pixX, np.zeros_like(pixY).astype(int)), surfaceNormalsMap.shape)] = foregroundNormalsArray[:, 0]
+	surfaceNormalsMapFlat[np.ravel_multi_index((pixY, pixX, np.ones_like(pixY).astype(int)), surfaceNormalsMap.shape)] = foregroundNormalsArray[:, 1]	
+	surfaceNormalsMapFlat[np.ravel_multi_index((pixY, pixX, 2 * np.ones_like(pixY).astype(int)), surfaceNormalsMap.shape)] = foregroundNormalsArray[:, 2]
+	surfaceNormalsMap = np.reshape(surfaceNormalsMapFlat, inputColor.shape) # reshape after that matlab imitation flattish thing
+
+	# Compute standard deviation of local normals
+	meanStdNormals = np.mean(window_stdev(surfaceNormalsMap, 25) * np.sqrt((25 ** 2) / (25 ** 2 - 1)), axis=2)
+	affordanceMap = 1 - meanStdNormals / np.max(meanStdNormals)
+	affordanceMap[validDepth != True] = 0
+	print("Finished predict!")
+	return affordanceMap, surfaceNormalsMap
+
+# from https://github.com/strawlab/python-pcl/blob/master/examples/sift.py
+def calc_surface_normals(cloud):
+    ne = cloud.make_NormalEstimation()
+    tree = cloud.make_kdtree()
+    ne.set_SearchMethod(tree)
+    ne.set_RadiusSearch(0.5)
+    cloud_normals = ne.compute()
+    return cloud_normals # returns an (N, 4) matrix because logic (last val is curvature I think)
--- src/suction_based_grasping/baseline/test.py
+++ src/suction_based_grasping/baseline/test.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pylab as plt
 
+# NOTE: assumes notebook is run from project directory
+from src.suction_based_grasping.baseline.predict import *
+
 def test():
 	data_path = '/home/jkailimcnelly/project/data'
 	filename = "{}/{}".format(data_path, "test-split.txt")
@@ -10,12 +13,45 @@ def test():
 	
 	results = {}
 	for img_name in test_split:
+		print("Working on image {}".format(img_name))
+
 		input_color = plt.imread("{}/color-input/{}.png".format(data_path, img_name))
 		input_depth = plt.imread("{}/depth-input/{}.png".format(data_path, img_name)) / 10000
 		background_color = plt.imread("{}/color-background/{}.png".format(data_path, img_name))
 		background_depth = plt.imread("{}/depth-background/{}.png".format(data_path, img_name)) / 10000
 		camera_intrinsics = np.loadtxt("{}/camera-intrinsics/{}.txt".format(data_path, img_name))
-		return input_color, input_depth, background_color, background_depth, camera_intrinsics
-		
+
+		plt.figure()
+		plt.imshow(input_color)
+		plt.axis('off')
+
+		plt.figure()
+		plt.imshow(input_depth)
+		plt.axis('off')
+
+		# plt.figure()
+		# plt.imshow(background_color)
+		# plt.axis('off')
+
+		# plt.figure()
+		# plt.imshow(background_depth)
+		# plt.axis('off')
+
+		# print(camera_intrinsics)
+
+		affordance_map, surface_normal_map = predict(input_color, input_depth, background_color, background_depth, camera_intrinsics)
+
+		plt.figure()
+		plt.imshow(affordance_map)
+		plt.axis('off')
+
+		# get plots to show live
+		plt.pause(0.05)
+		plt.show()
+
+		results[img_name] = affordance_map
+
+	return results
+
 
 
